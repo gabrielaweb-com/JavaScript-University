6db347345f9a6529b642790d6787760c875f1502
Gabriela Favaretto <paesfavaretto@gmail.com>
2025-12-10 21:15:50 -0300

    Create script.js

---
+++ projects/project1/script.js
@@ -0,0 +1,25 @@
+const melhorGuitarrista = "Jimi Hendrix";
+
+let a = 1;
+b = 2;
+c = 3;
+
+let salarioMinimo = 1518.0;
+
+//Diferença entre Let e Const:
+/*
+
+Const: é utilizado para valores que não devem ser alterados.
+Let: é utilizado para valores que podem ser alterados.
+*/
+
+//Exemplos:
+const gabriela = "Gabriela";
+// Outro nome daria erro no const
+let idade = 19;
+idade = 20;
+//é aceito no let
+
+//Porque utilizar var ou variáveis não declaradas é uma má prática?
+/* Var: Não possui escopo de bloco, o que pode gerar falhas na programação.
+Variáveis não declaradas: Torna a manutenção do código mais difícil por serem criadas no escopo global.
